d16755ddd0287ca4edc7c1fe0986904e93b5852a
Jacek Malec <malec@prisma.io>
2025-03-05 13:17:11 +0000

    fix: correct path

--- binaries/pkg/run.sh
+++ binaries/pkg/run.sh
@@ -8,5 +8,4 @@ pnpm install
 ENGINES_PACKAGE=$(node -e "console.log(path.dirname(require.resolve('@prisma/engines/package.json', {paths: [path.dirname(require.resolve('prisma/package.json'))]})))")
 
 # This is a hack to work around a problem that pkg would otherwise have with the Engines binaries missing in the package
-mkdir -p node_modules/prisma/node_modules/@prisma
-cp -fr $ENGINES_PACKAGE node_modules/prisma/node_modules/@prisma || echo "Could not copy engines package"
+find $ENGINES_PACKAGE -type f -exec cp -a {} node_modules/prisma/build/ \;
