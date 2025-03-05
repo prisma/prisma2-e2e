5449fc20c5120bb98b90301abfec749c1b1db25d
Jacek Malec <malec@prisma.io>
2025-03-05 15:38:22 +0000

    fix: correct external again

--- driver-adapters/turso-lambda-basic/run.sh
+++ driver-adapters/turso-lambda-basic/run.sh
@@ -15,7 +15,7 @@ GENERATED_CLIENT=$(node -e "
   )
 ")
 
-pnpm esbuild index.js --bundle --platform=node --target=node18 --outfile=dist/index.js --format=cjs --external:libsql/client
+pnpm esbuild index.js --bundle --platform=node --target=node18 --outfile=dist/index.js --format=cjs --external:@libsql/client
 cp "$GENERATED_CLIENT"/libquery_engine-rhel-openssl-1.0.x.so.node dist
 cp "$GENERATED_CLIENT"/schema.prisma dist
 
