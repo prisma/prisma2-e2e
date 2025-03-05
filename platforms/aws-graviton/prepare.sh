149fb77eb802b32956fdf14908db4f96712fc306
Jacek Malec <malec@prisma.io>
2025-03-05 12:34:28 +0000

    chore: verbose ssh

--- platforms/aws-graviton/prepare.sh
+++ platforms/aws-graviton/prepare.sh
@@ -4,6 +4,7 @@ set -eu
 
 echo "Disabling StrictHostKeyChecking"
 mkdir -p ~/.ssh
+ssh -vvv ec2-54-209-135-27.compute-1.amazonaws.com
 ssh-keyscan -vvv ec2-54-209-135-27.compute-1.amazonaws.com >> ~/.ssh/known_hosts
 echo "$SSH_KEY_GRAVITON" > ./server-key.pem
 
